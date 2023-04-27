acd75fac0a7f098a0ac15c92952990eb45893361
Sergiy Mironenko <falsyfox@gmail.com>
2023-04-27 12:22:48 +0300

    123

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const PRECACHE = 'cache-v3';
+const RUNTIME = 'runtime-1';
+
+const PRECACHE_URLS = [
+  'index.html',
+  './',
+  'favicon.ico',
+  'taxi-cab.png',
+  '/assets/index-1da8cd0c.css',
+  '/assets/index-a911f458.js',
+  '/assets/logo-277e0e97.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin) && self.location.hostname !== 'localhost') {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
